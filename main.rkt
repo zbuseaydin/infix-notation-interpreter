7fa3e2bff38e30adc4152d4194e8bc791c536718
zbuseaydin <88784379+zbuseaydin@users.noreply.github.com>
2022-05-26 14:05:54 +0300

    Add files via upload

---
+++ main.rkt
@@ -0,0 +1,62 @@
+#lang racket
+
+(provide (all-defined-out))
+
+; 10 points
+(define := (lambda (var value) (list var value)))
+
+
+; 10 points
+(define -- (lambda args (list 'let args)))
+
+
+; 10 points
+(define @ (lambda (parsed_binding expr)
+    (if (null? parsed_binding) expr (cons (car parsed_binding) (@ (cdr parsed_binding) expr)))))
+
+
+; 20 points
+
+
+(define split_at_delim (lambda (delim args) 
+    (if (null? args) '()
+        (if (equal? (car args) delim)
+            (list (split_at_delim delim (cdr args)))
+            (cons (car args) (split_at_delim delim (cdr args)))
+        )
+    )
+))
+
+
+
+(define (split-by args delim)
+  (foldr (lambda (element next)
+           (if (eqv? element delim)
+               (cons '() next)
+               (cons (cons element (car next)) (cdr next))))
+         (list '()) args))
+
+; 30 points
+
+(define (split-and-prefix 
+    (expr operand) (list operand (split-by expr operans)) 
+))
+
+(define parse_binding_list  )
+
+(define parse_expr (lambda (expr) 
+
+    (cond (member '+ expr) (parse_expr (split-and-prefix expr '+))
+        (member '* expr) (parse_expr (split-and-prefix expr '*))
+        (member '@ expr) ()
+        (number? expr) (expr) 
+        (atom? expr) (expr) 
+
+        (else expr)
+    )
+
+
+
+))
+; 20 points
+(define eval_expr (lambda (expr) 0))
